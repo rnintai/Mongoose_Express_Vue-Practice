5b54c938330abd60e52e94ba847206dd70eeff3b
rnintai <ho46825@gmail.com>
2021-05-05 00:27:47 +0900

    0505 00:27 - delete 구현중. _id로 삭제하기

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,46 +1,54 @@
 <template>
   <div class="home">
-    <div class="modal-bg" v-if="modalOn"><modal /></div>
-    <span v-if="loggedIn">
-      <span>{{ userInfo.name }}</span>
-      <br />
-      <b-button variant="outline-primary" @click="openModal">Add</b-button>
-      <div class="post-wrap" v-for="item in userInfo.post" :key="item.index">
-        {{ item.title }}
-        {{ item.content }}
-        {{ item.createdAt }}
+    <div class="modal-bg" v-if="modalOn"><add-modal /></div>
+    <div v-if="loggedIn" class="logged-in">
+      <div class="my-page">
+        <span>{{ userInfo.name }}'s Post</span>
+        <b-button variant="outline-primary" @click="openModal">Add</b-button>
       </div>
-    </span>
+      <post-box :post="userInfo.post"></post-box>
+    </div>
     <span v-else>로그인이 필요한 서비스입니다.</span>
   </div>
 </template>
 
 <script>
 import { mapState, mapMutations, mapActions } from "vuex";
-import Modal from "@/components/Modal.vue";
+import AddModal from "@/components/modals/AddModal.vue";
+import PostBox from "@/components/Home/PostBox.vue";
 // @ is an alias to /src
 export default {
-  components: { Modal },
+  components: { AddModal, PostBox },
   name: "Home",
   computed: {
     ...mapState(["loggedIn", "userInfo", "modalOn"]),
   },
   methods: {
-    ...mapActions("addpost", ["addPost"]),
     ...mapMutations(["openModal"]),
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.modal-bg {
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  top: 0%;
-  left: 0%;
-  transition: all 0.2s ease-in-out;
-  backdrop-filter: blur(5px);
-  background: rgba(0, 0, 0, 0.3);
+.home {
+  .modal-bg {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0%;
+    left: 0%;
+    backdrop-filter: blur(5px);
+    background: rgba(0, 0, 0, 0.3);
+    transition: all 0.3s ease-in-out;
+  }
+  .logged-in {
+    .my-page {
+      margin: 8px 0;
+      padding: 0 8px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+  }
 }
 </style>
